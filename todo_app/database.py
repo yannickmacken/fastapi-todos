f4e2b68df27e7584e940cf821062095746dcf1fb
ymacken <yannickmacken@gmail.com>
2022-05-25 17:34:53 +0200

    postgresql

--- todo_app/database.py
+++ todo_app/database.py
@@ -3,10 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # An Engine, which the Session will use for connection resources, specifying location and type of database
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:yann-RO-1616@localhost/TodoApplicationDatabase"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 # One central factory method to create a session. When SessionLocal() is called,
 # this method creates a session, so it is not necessary to pass engine every time
